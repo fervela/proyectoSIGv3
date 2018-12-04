6431fe9d7f7cbb4c802567c12ee54162afd0af39
BlueNet <luiyer1920@gmail.com>
2018-12-04 00:47:16 -0400

    cambios ubicacions

--- app/Taxi.php
+++ app/Taxi.php
@@ -3,9 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Taxi extends Model
 {
     use SoftDeletes;
     protected $dates = ['delete_at'];
+
+    public function ubicaciones(){
+    	return $this->hasMany('App\Ubicacion','taxi');
+    }
 }
--- app/Ubicacion.php
+++ app/Ubicacion.php
@@ -3,10 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class Ubicacion extends Model
 {
     protected $fillable = [
         'longitud', 'latitud', 'velocidad', 'taxi'
     ];
+
+    public function taxi(){
+    	return $this->belongsTo('App\Taxi');
+    }
 }
