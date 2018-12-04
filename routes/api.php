81677f8bf82e696e732cac5b49a9a8f30a5cdb72
alex <a_lexitox123@hotmail.com>
2018-12-04 01:49:20 -0300

    cambio de alex - 1:49

--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,13 @@ Route::post('/registrarUbicacion','UbicacionController@store');
 Route::get('/registrarUbicacion/{id}','UbicacionController@show');
 Route::get('/registrarUbicacion/','UbicacionController@showAll');
 Route::get('/getTaxis','VisualizarMapa@showTaxiStatus');
+
+//PARA EL PASAJERO
+Route::post('registrarPasajero',['as'=>'registrarPasajero','uses'=>'userController@registrarPasajero']);
+Route::post('loginPasajero',['as'=>'loginPasajero','uses'=>'userController@loginPasajero']);
+Route::post('obtenerToken',['as'=>'obtenerToken','uses'=>'userController@obtenerToken']);
+Route::post('notificarChofer',['as'=>'notificarChofer','uses'=>'userController@notificarChofer']);
+Route::post('regUbicacionTaxi',['as'=>'regUbicacionTaxi','uses'=>'UbicacionController@regUbicacionTaxi']);
+//vladimir
+Route::post('updateEstadoChofer',['as'=>'updateEstadoChofer','uses'=>'TaxiController@updateEstadoChofer']);
+Route::post('notificarPasajero',['as'=>'notificarPasajero','uses'=>'SolicitudController@notificarPasajero']);
